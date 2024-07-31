11af8f0b21628e42ad5d241e64ac74a1a6ab129f
fajrul.falah28 <fajrul.falah28@gmail.com>
2024-07-31 10:09:10 +0000

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  server: {
-    port: 3000
-  }
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        history: resolve(__dirname, 'history.html'),
+        implementation: resolve(__dirname, 'Implementation.html'),
+        source: resolve(__dirname, 'source.html'),
+        team: resolve(__dirname, 'team.html'),
+      },
+    },
+  },
 });
